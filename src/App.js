70a25f9d4e1f2992ecb781f4a4276bccf1b21520
zasalast <zasalas60@gmail.com>
2022-01-03 15:25:22 -0500

    [**Nuevo proyecto: códigos de seguridad**](https://www.notion.so/Nuevo-proyecto-c-digos-de-seguridad-c5902a0a09e6496bb03958a696048188)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+ 
 import './App.css';
-
+import { ClassState } from './containers/ClassState/ClassState'
+import { UseState} from './containers/UseState/UseState'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">     
+       Header
       </header>
+      <main className="App-main">
+        <UseState />
+        <ClassState/>
+      </main>
+
+      <footer className="App-footer">
+        footer
+      </footer>
     </div>
   );
 }
---
+++ src/containers/UseState/UseState.js
@@ -0,0 +1,11 @@
+const UseState = () => {
+    return <> 
+ 
+      <h2>Eliminar UseState</h2>
+      <p>Por favor, escriba el código de seguridad.</p>
+      <input type='text' placeholder='código de seguridad'/>
+      <button>Comprobar</button>
+     
+  </>
+}
+export {UseState}
