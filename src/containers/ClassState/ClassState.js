80804d909f8e879df5a0004fceb3e7899b28e065
DesarrolloColectivo <zasalastobon@gmail.com>
2022-01-10 15:00:23 -0500

    validate SEGURITY_CODE of ClassState

--- src/containers/ClassState/ClassState.js
+++ src/containers/ClassState/ClassState.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import {Loading} from './Loading'
+const SECURITY_CODE = "paradigma";
 class ClassState extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: true,
+            value:'',
+            error: false,
             loading:false
         }
     }
@@ -14,7 +16,15 @@ class ClassState extends Component {
         if (this.state.loading) {
             setTimeout(() => {
                 console.log("Haciendo la validación");
-                this.setState({loading:!this.state.loading});
+
+                if (this.state.value !== SECURITY_CODE) {
+                    this.setState({error:true,loading:!this.state.loading});
+                     
+                  }else{
+                    this.setState({error:false,loading:!this.state.loading});
+                  }
+                  
+                
                 console.log("fin la validación");
               }, 3000);
         }
@@ -23,13 +33,18 @@ class ClassState extends Component {
         () {
             return <> <h2>Eliminar { this.props.name}</h2>
         <p>Por favor, escriba el código de seguridad.</p>
-        {this.state.error && ( 
+        {(this.state.error && !this.state.loading) && ( 
             <p> Error: el código es incorrecto</p>
         )}
+        
          {this.state.loading && ( 
             <Loading/>
         )}
-                <input type='text' placeholder='código de seguridad' />
+                <input type='text'
+                 placeholder='código de seguridad'
+                  value={this.state.value}
+                  onChange={(event)=>{this.setState({value:event.target.value})}}
+                  />
                 <button onClick={() => this.setState(prevState=>({ loading:!prevState.loading }))}>
                 Comprobar</button>
         </>}
